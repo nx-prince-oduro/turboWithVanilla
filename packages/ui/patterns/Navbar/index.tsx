bec7629ad042291da4b4a2d00fd5b469fb68bea6
Pius Prince <prince.oduro@nexum.com>
2023-01-19 11:57:07 +0000

    feat: Add new links

--- packages/ui/patterns/Navbar/index.tsx
+++ packages/ui/patterns/Navbar/index.tsx
@@ -6,8 +6,8 @@ const Navbar = () => {
       <h1>Igus</h1>
       <div className={navLinks}>
         <a>Contact</a>
-        <a>Link 2</a>
-        <a>Link 3</a>
+        <a>About</a>
+        <a>Support</a>
       </div>
     </div>
   );
